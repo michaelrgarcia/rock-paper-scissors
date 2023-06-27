07de82aefae8622a114d23ea8f4bb22276adfb4d
leavesturninsideyou <134650278+leavesturninsideyou@users.noreply.github.com>
2023-06-27 13:03:54 -0700

    Create "game round" function
    This function will handle each round of the upcoming game function.

--- javascript.js
+++ javascript.js
@@ -1,4 +1,27 @@
-console.log(getComputerChoice());
+const playerSelection = prompt("Rock, paper, or scissors?");
+const computerSelection = getComputerChoice();
+
+console.log(playRound(playerSelection.toLowerCase(), computerSelection.toLowerCase()));
+
+function playRound(playerSelection, computerSelection) { 
+        if (playerSelection === computerSelection) {
+            return "Try again!";
+        } else if (playerSelection === "rock" && computerSelection === "paper") {
+            return "You lose, paper beats rock!";
+        } else if (playerSelection === "rock" && computerSelection === "scissors") {
+            return "You win, rock beats paper!";
+        } else if (playerSelection === "paper" && computerSelection === "rock") {
+            return "You win, paper beats rock!";
+        } else if (playerSelection === "paper" && computerSelection === "scissors") {
+            return "You lose, scissors beats paper!";
+        } else if (playerSelection === "scissors" && computerSelection === "rock") {
+            return "You lose, rock beats scissors!";
+        } else if (playerSelection === "scissors" && computerSelection === "paper") {
+            return "You win, scissors beats paper!";
+        } else {
+            return "Invalid input! Only rock, paper, or scissors are allowed!";
+        }
+}
 
 function getComputerChoice() {
     const computerChoice = Math.floor(Math.random() * 3) + 1;
