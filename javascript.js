f4ec103050f0caac76efb534a17e82c6c6a55f40
leavesturninsideyou <134650278+leavesturninsideyou@users.noreply.github.com>
2023-06-27 11:37:08 -0700

    Create getComputerChoice() function
    This function randomly outputs "rock", "paper", or "scissors".

---
+++ javascript.js
@@ -0,0 +1,12 @@
+console.log(getComputerChoice());
+
+function getComputerChoice() {
+    const computerChoice = Math.floor(Math.random() * 3) + 1;
+    if (computerChoice === 1) {
+        return "Rock";
+    } else if (computerChoice === 2) {
+        return "Paper";
+    } else {
+        return "Scissors";
+    }
+}
