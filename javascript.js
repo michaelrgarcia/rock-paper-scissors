365375d8bde629cab33bef35b2ea2070137e0db2
leavesturninsideyou <134650278+leavesturninsideyou@users.noreply.github.com>
2023-07-06 13:23:45 -0700

    Finish rudimentary stage of game
    Hard to put this one in one line. A lot of improvement left but I finished the basic results displaying and made the choice buttons/reset button work when the game is on.

--- javascript.js
+++ javascript.js
@@ -1,7 +1,10 @@
-const choicebtns = document.querySelectorAll(".choicebtns button")
+const choicebtns = document.querySelectorAll(".choicebtns button");
 const results = document.querySelector(".results");
+const gamectrl = document.querySelectorAll(".gamectrl button");
 let computerScore = 0;
 let playerScore = 0;
+let game = false;
+let playerSelection = "";
 
 /* for (let i = 0; i < 5; i++) {
     console.log(playRound());
@@ -18,10 +21,21 @@ let playerScore = 0;
 
 */
 
+gamectrl.forEach((btn) => {
+    btn.addEventListener("click", () => {
+        if (btn.className === "start") {
+            game = true;
+            results.textContent = "Pick one!";
+        } else if (btn.className == "reset" && game) {
+            playerSelection = "";
+            results.textContent = "Pick one!";
+        }
+    });
+});
+
 choicebtns.forEach((btn) => {
     btn.addEventListener("click", () => {
-        let playerSelection = "";
-        const computerSelection = getComputerChoice();
+    if (game) {
         if (btn.className === "rock") {
             playerSelection = "rock"
         } else if (btn.className === "paper") {
@@ -29,13 +43,48 @@ choicebtns.forEach((btn) => {
         } else if (btn.className === "scissors") {
             playerSelection = "scissors"
         }
-        results.textContent = `You selected ${playerSelection}.`
-        return playerSelection;
+        playRound2();
+    }
     });
 });
 
+function playRound2() {
+    const computerSelection = getComputerChoice();
+        if (playerSelection === computerSelection) {
+            results.textContent = "Try again!";
+        } else if (playerSelection === "rock" && computerSelection === "paper") {
+            computerScore += 1;
+            results.textContent = "You lose, paper beats rock!";
+        } else if (playerSelection === "rock" && computerSelection === "scissors") {
+            playerScore += 1;
+            results.textContent = "You win, rock beats paper!";
+        } else if (playerSelection === "paper" && computerSelection === "rock") {
+            playerScore += 1;
+            results.textContent = "You win, paper beats rock!";
+        } else if (playerSelection === "paper" && computerSelection === "scissors") {
+            computerScore += 1;
+            results.textContent = "You lose, scissors beats paper!"; 
+        } else if (playerSelection === "scissors" && computerSelection === "rock") {
+            computerScore += 1;
+            results.textContent = "You lose, rock beats scissors!"; 
+        } else if (playerSelection === "scissors" && computerSelection === "paper") {
+            playerScore += 1;
+            results.textContent = "You win, scissors beats paper!";
+        }
+}
 
-function playRound(playerSelection, computerSelection) { 
+function getComputerChoice() {
+    const computerChoice = Math.floor(Math.random() * 3) + 1;
+    if (computerChoice === 1) {
+        return "rock";
+    } else if (computerChoice === 2) {
+        return "paper";
+    } else {
+        return "scissors";
+    }
+}
+
+/* function playRound(playerSelection, computerSelection) { 
         if (playerSelection === null || playerSelection === "") {
             return "You didn't input anything!";
         } else if (playerSelection === computerSelection) {
@@ -62,14 +111,4 @@ function playRound(playerSelection, computerSelection) {
             return "Invalid input! Only rock, paper, or scissors are allowed!";
         }
 }
-
-function getComputerChoice() {
-    const computerChoice = Math.floor(Math.random() * 3) + 1;
-    if (computerChoice === 1) {
-        return "rock";
-    } else if (computerChoice === 2) {
-        return "paper";
-    } else {
-        return "scissors";
-    }
-}
+*/
