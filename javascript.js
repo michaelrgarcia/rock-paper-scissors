915d613b3f42937f70264bd71af4ff9917b68cb1
leavesturninsideyou <134650278+leavesturninsideyou@users.noreply.github.com>
2023-07-06 22:12:25 -0700

    Finish game mechanics / display mechanism
    I still need to find a way to make playerSelection not global.

--- javascript.js
+++ javascript.js
@@ -1,34 +1,23 @@
 const choicebtns = document.querySelectorAll(".choicebtns button");
 const results = document.querySelector(".results");
 const gamectrl = document.querySelectorAll(".gamectrl button");
+const score = document.querySelector(".score");
 let computerScore = 0;
 let playerScore = 0;
 let game = false;
 let playerSelection = "";
-
-/* for (let i = 0; i < 5; i++) {
-    console.log(playRound());
-    console.log(`You: ${playerScore} \nConsole: ${computerScore}`);
-}
-
- if (playerScore > computerScore) {
-    console.log("You win the game!");
-} else if (playerScore === computerScore) {
-    console.log("It's a tie!");
-} else {
-    console.log("Console wins the game!");
-}
-
-*/
 
 gamectrl.forEach((btn) => {
     btn.addEventListener("click", () => {
         if (btn.className === "start") {
             game = true;
             results.textContent = "Pick one!";
-        } else if (btn.className == "reset" && game) {
-            playerSelection = "";
+        } else if (btn.className == "reset" && playerSelection != "") {
             results.textContent = "Pick one!";
+            playerSelection = "";
+            score.textContent = "";
+            playerScore = 0;
+            computerScore = 0;
         }
     });
 });
@@ -43,18 +32,19 @@ choicebtns.forEach((btn) => {
         } else if (btn.className === "scissors") {
             playerSelection = "scissors"
         }
-        playRound2();
+        playRound();
+        checkScore();
     }
     });
 });
 
-function playRound2() {
+function playRound() {
     const computerSelection = getComputerChoice();
         if (playerSelection === computerSelection) {
-            results.textContent = "Try again!";
+            results.textContent = "You both picked the same option, pick again!";
         } else if (playerSelection === "rock" && computerSelection === "paper") {
             computerScore += 1;
-            results.textContent = "You lose, paper beats rock!";
+            results.textContent = `You lose, paper beats rock!`;
         } else if (playerSelection === "rock" && computerSelection === "scissors") {
             playerScore += 1;
             results.textContent = "You win, rock beats paper!";
@@ -73,6 +63,22 @@ function playRound2() {
         }
 }
 
+
+function checkScore() {
+    score.textContent = `\nYou: ${playerScore} Computer: ${computerScore}`;
+    if (playerScore === 5 || computerScore === 5) {
+        score.textContent = "Click 'Reset' to play again!";
+        game = false;
+        if (playerScore > computerScore) {
+            results.textContent = "You win the game!";
+        } else if (playerScore === computerScore) {
+            results.textContent = "It's a tie!";
+        } else {
+            results.textContent = "Console wins the game!";
+        }
+    }
+}
+
 function getComputerChoice() {
     const computerChoice = Math.floor(Math.random() * 3) + 1;
     if (computerChoice === 1) {
@@ -83,32 +89,3 @@ function getComputerChoice() {
         return "scissors";
     }
 }
-
-/* function playRound(playerSelection, computerSelection) { 
-        if (playerSelection === null || playerSelection === "") {
-            return "You didn't input anything!";
-        } else if (playerSelection === computerSelection) {
-            return "Try again!";
-        } else if (playerSelection === "rock" && computerSelection === "paper") {
-            computerScore += 1;
-            return "You lose, paper beats rock!";
-        } else if (playerSelection === "rock" && computerSelection === "scissors") {
-            playerScore += 1;
-            return "You win, rock beats paper!";
-        } else if (playerSelection === "paper" && computerSelection === "rock") {
-            playerScore += 1;
-            return "You win, paper beats rock!";
-        } else if (playerSelection === "paper" && computerSelection === "scissors") {
-            computerScore += 1;
-            return "You lose, scissors beats paper!"; 
-        } else if (playerSelection === "scissors" && computerSelection === "rock") {
-            computerScore += 1;
-            return "You lose, rock beats scissors!"; 
-        } else if (playerSelection === "scissors" && computerSelection === "paper") {
-            playerScore += 1;
-            return "You win, scissors beats paper!";
-        } else {
-            return "Invalid input! Only rock, paper, or scissors are allowed!";
-        }
-}
-*/
